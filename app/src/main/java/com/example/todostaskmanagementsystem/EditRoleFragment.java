ddf0c10902a3d351173a1f01dfe172a8c1807b9d
mayyan <mayyan050700@gmail.com>
2021-11-03 23:26:42 +0800

    ui

---
+++ app/src/main/java/com/example/todostaskmanagementsystem/EditRoleFragment.java
@@ -0,0 +1,41 @@
+package com.example.todostaskmanagementsystem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.todostaskmanagementsystem.model.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class EditRoleFragment extends Fragment {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public EditRoleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_edit_role, container, false);
+        Button btn = view.findViewById(R.id.btn_saveRole);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/todostaskmanagementsystem/ManageRoleFragment.java
+++ app/src/main/java/com/example/todostaskmanagementsystem/ManageRoleFragment.java
@@ -3,13 +3,23 @@ package com.example.todostaskmanagementsystem;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.todostaskmanagementsystem.model.Role;
+import com.example.todostaskmanagementsystem.model.Todolist;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class ManageRoleFragment extends Fragment {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private ArrayList<Role> roles = new ArrayList();
 
     public ManageRoleFragment() {
         // Required empty public constructor
@@ -25,11 +35,12 @@ public class ManageRoleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_manage_role, container, false);
-        Button btn = view.findViewById(R.id.btn_memberSave);
+        Button btn = view.findViewById(R.id.button_create_role);
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_roles);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                NavHostFragment.findNavController(getParentFragment()).navigate(ManageRoleFragmentDirections.actionManageRoleFragmentToCreateRoleFragment());
             }
         });
         return view;
